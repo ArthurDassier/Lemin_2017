cc008e068289839ef35c128dc60ccbff8505fbf7
Angela Martoccia <boyadjian.angela@gmail.com>
2018-04-04 13:51:17 +0200

    merge staging

--- srcs/main.c
+++ srcs/main.c
@@ -10,14 +10,11 @@ int main(int ac, char **av)
 {
 	t_node *node = NULL;
 
-	create_graph(&node, 4);
+	create_graph(&node, 6);
 	(void)ac;
 	(void)av;
-	add_edge(node, 1, 2);
-	add_edge(node, 2, 3);
-	add_edge(node, 1, 3);
-	add_edge(node, 2, 1);
-	add_edge(node, 3, 2);
+	add_edge(node, 1, 5);
+	add_edge(node, 6, 4);
 	print_graph(node);
 	//display_list(node, &print_list);
 	return (0);
