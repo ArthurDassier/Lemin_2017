1a726132095ec20e4c0cac9648459a9149f6bd03
Vincent <vincent.lemesle@epitech.eu>
2018-04-04 15:37:38 +0200

    begin create graph

--- srcs/gen_graph/gen_graph.c
+++ srcs/gen_graph/gen_graph.c
@@ -7,48 +7,59 @@
 
 #include "lemin.h"
 
-// static int find_nb_vertices(char **names)
-// {
-// 	int	i = 0;
+static int find_nb_vertices(t_node *room)
+{
+	t_node	*tmp_node = room;
+	t_room	*tmp_room = NULL;
+	int	vertices = 0;
 
-// 	while (names[i])
-// 		++i;
-// 	return (i);
-//
- static void remp_rooms_info(t_graph *graph, t_room **room)
- {
- 	t_room		*tmp_room = NULL;
- 	t_adj_node	*tmp = NULL;
- 	int		i = 0;
- 	int		nb = 0
- 	while (room[i]) {
- 		nb = room[i]->nb_room;
- 		tmp = graph->array[nb].head;
- 		graph->array[nb].head->data = malloc(sizeof(t_room));
- 		tmp_room = (t_room *)tmp->data;
- 		tmp_room->ant = room[i]->ant;
- 		tmp_room->x = room[i]->x;
- 		tmp_room->y = room[i]->y;
- 		tmp_room->type = room[i]->type;
- 		tmp_room->nb_room = room[i]->nb_room;
- 		++i;
- 	}
+	room = room->prev;
+	tmp_room = (t_room *)room->data;
+	vertices = tmp_room->nb_room;
+	room = tmp_node;
+	return (vertices);
 }
-//
-// t_graph	*gen_graph(t_infos *infos)
-// {
-// 	t_graph		*graph =
-// 	create_graph(find_nb_vertices(infos->tunnels->names));
-// 	int		i = 0;
 
-// 	while (infos->tunnels->tunnels[i]) {
-// 		add_edge(graph, infos->tunnels->tunnels[i][0],
-// 		infos->tunnels->tunnels[i][1]);
-// 		++i;
-// 	}
-// 	remp_rooms_info(graph, infos->rooms);
-// 	free(infos);
-// 	free(infos->tunnels);
-// 	free(infos->rooms);
-// 	return (graph);
-// }
+static void remp_rooms_info(t_node **graph, t_node *room_list)
+{
+	t_room		*graph_room = NULL;
+	t_room		*room_index = NULL;
+	t_node		*tmp_node = room_list;
+	int		i = 0;
+
+	do {
+		room_index = (t_room *)room_list->data;
+		i = room_index->nb_room;
+		tmp_node = graph[i];
+		graph[i]->data = malloc(sizeof(t_room));
+		graph_room = (t_room *)tmp_node->data;
+		graph_room->ant = false;
+		graph_room->type = room_index->type;
+		graph_room->nb_room = room_index->nb_room;
+		room_list = room_list->next;
+	} while (room_list != tmp_node);
+}
+
+static void gen_edges(t_node *graphi, t_node *tunnels)
+{
+	t_node	*tmp_tun = tunnels;
+	int	*tmp_int = NULL;
+
+	tunnels = tunnels->next;
+	while (tunnels != tmp_tun) {
+		tmp_int = (int *)tunnels->data;
+		add_edge(graphi, tmp_int[0], tmp_int[1]);
+		tunnels = tunnels->next;
+	}
+}
+
+t_node	*gen_graph(t_infos *infos)
+{
+	t_node		*graphi = malloc(sizeof(t_node));
+
+	graphi->graph = malloc(sizeof(t_node *) * find_nb_vertices(infos->rooms));
+	create_graph(graphi->graph, find_nb_vertices(infos->rooms));
+	gen_edges(graphi, infos->tunnels);
+	remp_rooms_info(graphi->graph, infos->rooms);
+	return (graphi);
+}
