4d96b7253f5c7732a146f938ea77973fc0ed1c43
Vincent <vincent.lemesle@epitech.eu>
2018-04-05 16:16:55 +0200

    add diplay room/tunnels

--- srcs/display/display.c
+++ srcs/display/display.c
@@ -7,16 +7,51 @@
 
 #include "lemin.h"
 
+static void print_room(t_room *room)
+{
+	if (room->type == 1)
+		my_printf("##start\n");
+	else if (room->type == 2)
+		my_printf("##end\n");
+	my_printf("%s", room->name_room);
+	my_printf("%d %d\n", room->x, room->y);
+}
+
+static int find_nb_of_ants(t_room *room)
+{
+	t_room	*tmp = room;
+
+	while (tmp->type != 1)
+		tmp = tmp->next;
+	return (tmp->ant);
+}
+
+static void print_tunnels(t_tunnels *tunnels)
+{
+	t_tunnels	*tmp = tunnels;
+	int		*data = NULL;
+
+	do {
+		data = (int *)tmp->data;
+		my_printf("%d-%d\n", data[0], data[1]);
+		tmp = tmp->next;
+	} while (tmp != tunnels);
+}
+
 void diaplay_infos(s_infos *infos, char **tab)
 {
 	t_room	*head = infos->rooms;
-	t_room	*i_room = NULL;
+	t_room	*room = NULL;
 
+	room = (t_room *)infos->rooms;
 	my_printf("#number_of_ants\n");
+	my_printf("%d\n", find_nb_of_ants(infos->rooms));
 	my_printf("#rooms\n");
 	do {
-		i_room = (t_room *)infos->data;
-		my_printf("%s", i_room->name_room);
-		my_printf("%d %d\n", i_room->x, i_room->y);
+		room = (t_room *)infos->data;
+		print_room(room);
 	} while (infos->rooms != head);
+	my_printf("#tunnels");
+	print_tunnels(infos->tunnels);
+	my_printf("#moves\n");
 }
