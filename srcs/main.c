9b3fbd1e9c632575a35d4cac260d45ad8fa9cf21
Arthur Dassier <arthur.dassier@epitech.eu>
2018-04-08 23:22:33 +0200

    l

--- srcs/main.c
+++ srcs/main.c
@@ -6,12 +6,15 @@
 */
 #include "lemin.h"
 
-int main(void)
+int main(int ac, char **av)
 {
 	char	**tab = NULL;
 	t_infos	*infos = NULL;
 	t_node	*node = NULL;
 
+	(void) av;
+	if (ac > 1)
+		return (FAILURE);
 	tab = recup_anthill(&infos, tab);
 	if (tab == NULL) {
 		if (print_err_rooms(infos->err_ant, infos->rooms) == FAILURE)
